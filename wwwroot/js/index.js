97fb74335d6ad2db32dad40011f96e5ad90c925b
Adam <adamjpolak@gmail.com>
2025-03-25 15:36:23 -0500

    remove index.js

--- wwwroot/js/index.js
+++
@@ -1,9 +0,0 @@
-const origin = window.location.origin;
-
-const secretMessage = document.getElementById("secret-message");
-
-fetch(origin + "/home/secret").then(resp => {
-    resp.text().then(body => {
-        secretMessage.innerText = body;
-    })
-});
