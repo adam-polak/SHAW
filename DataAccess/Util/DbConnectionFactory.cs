7378a022e8526c8ed272747fc5113c0ce5818a8e
Adam <adamjpolak@gmail.com>
2025-04-22 11:43:15 -0500

    add setup readme and add todo to connection factory

--- DataAccess/Util/DbConnectionFactory.cs
+++ DataAccess/Util/DbConnectionFactory.cs
@@ -6,21 +6,46 @@ namespace SHAW.DataAccess.Util;
 
 public static class DbConnectionFactory
 {
-    public static DbConnection CreateDbConnection(IHostEnvironment env)
+    private static string CreateMySqlConnectionString(string username, string password)
     {
-        string? connectionString;
-        if(env.IsDevelopment())
-        {
-            connectionString = JsonHelper.GetJsonSecret("DatabaseConnectionString");
-        } else {
-            connectionString = Environment.GetEnvironmentVariable("DatabaseConnectionString");
-        }
+        return $"mysql://{username}:{password}@localhost:3306/shawdb";
+    }
 
-        if(connectionString == null)
+    private static DbConnection TryCreateDbConnection(IHostEnvironment env, bool firstTime)
+    {
+        string? username = JsonHelper.GetJsonSecret("DatabaseUsername");
+        string? password = JsonHelper.GetJsonSecret("DatabasePassword");
+
+        if(username == null || password == null)
         {
             throw new Exception("Missing DatabaseConnectionString environment variable");
         }
 
-        return new MySqlConnection(connectionString);
+        DbConnection? connection;
+        string connectionString = CreateMySqlConnectionString(username, password);
+        try {
+            connection = new MySqlConnection(connectionString);
+        } catch(Exception e) {
+            /*
+             either server isn't started or "shawdb" hasn't been set up
+             **TODO**
+
+                 -determine which error appears for which case
+                     - case server isn't started, throw a real exception stopping the program so that
+                        the user can start the server
+
+                     -case "shawdb" hasn't been setup, perform a setup then retry connection
+            */
+            Console.WriteLine(e);
+            // IDEA: recursive call, with "firstTime" set to false so it won't recursively call anymore
+            return TryCreateDbConnection(env, false);
+        }
+        
+        return connection;
+    }
+
+    public static DbConnection CreateDbConnection(IHostEnvironment env)
+    {
+        return TryCreateDbConnection(env, true);
     }
 }
