c09ed5184b689555c0cdbf9d323fbe12ac7d301a
Adam <adamjpolak@gmail.com>
2025-05-07 15:02:58 -0500

    fix warnings

--- DataAccess/Models/PostsModel.cs
+++ DataAccess/Models/PostsModel.cs
@@ -3,8 +3,8 @@ namespace SHAW.DataAccess.Models;
 public class PostsModel
 {
     public int Id { get; set; }
-    public string Title { get; set; }
-    public string Body { get; set; }
+    public required string Title { get; set; }
+    public required string Body { get; set; }
     public DateTime CreatedOn { get; set; }
-    public string Author { get; set; }
+    public required string Author { get; set; }
 }
